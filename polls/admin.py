99e8138c03dcdf7e494eb6f204e63c7dd58ae12c
Andrew Minarick <andrew.minarick@gmail.com>
2018-06-20 23:12:00 -0500

    fixed models

--- polls/admin.py
+++ polls/admin.py
@@ -1,42 +1,48 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-from .models import Question, Choice, Listing, Products, Cart
+from .models import Question, Choice, Listing, Products, Cart, Order
 
 
 
 # Register your models here.
 
 
-class ChoiceInline(admin.TabularInline):
-    model = Choice
-    extra = 3
+# class ChoiceInline(admin.TabularInline):
+#     model = Choice
+#     extra = 3
+#
+#
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['question_text']}),
+#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#     ]
+#     inlines = [ChoiceInline]
+#
+#
+# class ProductsInline(admin.TabularInline):
+#     model = Products
+#     extra = 3
+#
+#
+# class ListingAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['listing_name']}),
+#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#     ]
+#     inlines = [ProductsInline]
+#
+# class OrderAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields': ['listing_name']}),
+#         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+#     ]
+#     inlines = [ProductsInline]
 
-
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-
-
-class ProductsInline(admin.TabularInline):
-    model = Products
-    extra = 3
-
-
-class ListingAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None, {'fields': ['listing_name']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ProductsInline]
-
-
-
-
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Listing, ListingAdmin)
+#
+admin.site.register(Question)
+admin.site.register(Listing)
 admin.site.register(Cart)
+admin.site.register(Order)
 
